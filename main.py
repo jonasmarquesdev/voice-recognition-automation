7786bac473b3aeb5018926c2e993e7f4ca3ffb3e
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-05-19 13:13:04 -0300

    Reconhecimento de voz

---
+++ main.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import pyaudio
+
+def main():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    while True:
+        try:
+            with mic as source:
+                print("Diga algo...")
+                audio = r.listen(source)
+
+            text = r.recognize_google(audio, language="pt-BR")
+            print("Você disse:", text)
+
+        except sr.UnknownValueError:
+            print("Não foi possível reconhecer o áudio.")
+        except sr.RequestError as e:
+            print("Erro na solicitação ao serviço de reconhecimento de voz: {0}".format(e))
+
+if __name__ == '__main__':
+    main()
