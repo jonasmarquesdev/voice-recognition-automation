beaaeed20cdf2d731de0d884d6ab105ed61f6262
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-05-19 13:41:16 -0300

    Automatizando tarefas mesclando Síntese de fala

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyaudio
 from voz import say
+import subprocess
 
 def main():
     r = sr.Recognizer()
@@ -15,6 +16,30 @@ def main():
 
             text = r.recognize_google(audio, language="pt-BR")
             print("Você disse:", text)
+
+            # Verifica se a palavra em (text) foi falada
+            # Substitua "nome_do_programa" pelo nome ou caminho do programa que deseja abrir
+            # subprocess.Popen(["nome_do_programa"])
+            
+            if "encerrar processo" in text.lower():
+                say("Encerrando processo")
+                break
+
+            elif "abrir steam" in text.lower():
+                say("Abrindo Steam")
+                subprocess.Popen(["C:\Program Files (x86)\Steam\steam.exe"])
+
+            elif "fechar steam" in text.lower():
+                say("Fechando Steam")
+                subprocess.Popen(["taskkill", "/F", "/IM", "steam.exe"])
+
+            elif "abrir bloco de notas" in text.lower():
+                say("Abrindo Bloco de notas")
+                subprocess.Popen(["notepad.exe"])
+            
+            elif "fechar steam" in text.lower():
+                say("Fechando Bloco de notas")
+                subprocess.Popen(["taskkill", "/F", "/IM", "notepad.exe"])
 
         except sr.UnknownValueError:
             print("Não foi possível reconhecer o áudio.")
